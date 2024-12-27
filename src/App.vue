e20765ccb6e2ef9e3a4ff57d5fba1e1dc07d3ad7
umemura <ume.daihuku0116.0707@gmail.com>
2024-12-27 14:18:01 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view />
+    <ApiTest></ApiTest>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ApiTest from './components/ApiTest.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ApiTest
   }
 }
 </script>
